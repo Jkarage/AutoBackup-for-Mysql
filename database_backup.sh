e72338329ff3da60d2c42aeebcb29c0265612695
root <root@linux.us-central1-a.c.vertical-orbit-280106.internal>
2020-10-07 15:22:42 +0000

    initial commit

---
+++ database_backup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This script will write a file  backup for your database.
+
+echo "Type your mysql username(Example root), followed by [ENTER]:"
+read  MYSQL_UNAME
+
+
+echo "Type the name  of the database you want to backup, followed by [ENTER]:"
+read DATABASE
+
+mysqldump -u $MYSQL_UNAME -p  $DATABASE >  /home/${DATABASE}backup.sql
