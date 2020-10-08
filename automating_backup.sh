dfc73f7e1c0ea52b63285bfd8da2cd1fc7630fc9
Jkarage <josephbkarage@gmail.com>
2020-10-08 16:30:13 +0000

    updating the file

--- automating_backup.sh
+++ automating_backup.sh
@@ -10,7 +10,7 @@ mkdir -p ${BACKUP_PATH}
 echo "Starting Backup for database - ${DATABASE_NAME}"
 
 # Using the mysqldump tool to dump the database into a file
-mysqldump  -u ${MYSQL_USERNAME} -p${MYSQL_PASSWD} ${DATABASE_NAME} > /home/backup/${DATABASE_NAME}-${now}.sql
+mysqldump  -u ${MYSQL_USERNAME} -p${MYSQL_PASSWD} ${DATABASE_NAME} > ${BACKUP_PATH}/${DATABASE_NAME}-${now}.sql
 
 # Check if the procedure had an error or not
 if [ $? -eq 0 ]; then
